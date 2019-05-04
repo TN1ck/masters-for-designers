46ef430633e3b53869860402616454993303eda8
Tom Nick <tomwanick@gmail.com>
2019-05-04 19:43:45 +0200

    feat: change title and set favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,8 @@ var proxy = require("http-proxy-middleware");
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
-    description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+    title: "Masters for Designers",
+    description: "Masters for Designers.",
   },
   plugins: [
     {
