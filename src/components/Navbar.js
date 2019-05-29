900aff80c49e59e028eb951f63c066510798f0c9
TN1ck <tomwanick@gmail.com>
2019-05-29 13:20:18 +0200

    fix: change font to regular on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -62,6 +62,9 @@ const NavbarLink = styled(Link)`
 const MainNavbarLink = styled(NavbarLink)`
   font-size: 18px;
   font-weight: bold;
+  @media (max-width: 700px) {
+    font-weight: 400;
+  }
 `;
 
 const NavbarLinks = styled.div`
