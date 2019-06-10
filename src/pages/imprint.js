7f5427f1477c0f4c96eb545a8406b41867c5214b
Tom Nick <tomwanick@gmail.com>
2019-06-10 20:31:45 +0200

    chore: update imprint

--- src/pages/imprint.js
+++ src/pages/imprint.js
@@ -13,6 +13,7 @@ const Imprint = ({data}) => {
       <Masthead background="white">
         <Container>
           <Headline>{"Impressum"}</Headline>
+          <small>Angaben gemäß § 5 TMG</small>
         </Container>
       </Masthead>
       <InformationPageContainer>
@@ -29,7 +30,7 @@ const Imprint = ({data}) => {
               10967 Berlin
               <br />
               <br />
-              +49 176 66 893 941
+              <a href="tel:+49 176 66 893 941">+49 176 66 893 941</a>
               <br />
               <a mailto="hello@mastersfordesigners.com">hello@mastersfordesigners.com</a>
             </ColumnHalf>
@@ -50,25 +51,25 @@ const Imprint = ({data}) => {
             <ColumnHalf>
               <h5>Haftung für Inhalte</h5>
               <p>
-                Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den
+                Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den
                 allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
-                verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu
-                forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+                verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu
+                forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
               </p>
               <p>
                 Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen
-                bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis
-                einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden
+                bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis
+                einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden
                 wir diese Inhalte umgehend entfernen.
               </p>
             </ColumnHalf>
             <ColumnHalf>
               <h5>Haftung für Links</h5>
               <p>
-                Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben.
-                Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+                Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben.
+                Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
                 verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die
-                verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
+                verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
                 Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
               </p>
               <p>
@@ -81,9 +82,9 @@ const Imprint = ({data}) => {
               <h5>Urheberrecht</h5>
               <p>
                 Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen
-                Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
-                Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
-                Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+                Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+                Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+                Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
               </p>
               <p>
                 Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte
